6f4599a8988301dab5d8dc382e6e24f1e71e759e
jadedResearcher <gtg287y@gmail.com>
2018-07-08 15:12:19 -0400

    styling

--- web/scripts/Gameplay/World.dart
+++ web/scripts/Gameplay/World.dart
@@ -5,7 +5,7 @@ import "package:DollLibCorrect/DollRenderer.dart";
 class World {
     int width = 800;
     int height = 1000;
-    String bgLocation = "images/BGs/AlternianCliffs.png";
+    String bgLocation = "images/BGs/AlternianCliff.png";
 
     List<TreeDoll> trees = new List<TreeDoll>();
 
@@ -29,20 +29,30 @@ class World {
     Future<Null> initCanvasAndBuffer() async {
         //graphic of branches holding it up, yggdrasil style
         onScreen = new CanvasElement(width: width, height:height);
+        onScreen.classes.add("worldBase");
         container.append(onScreen);
         bg = await Loader.getResource(bgLocation);
         branches = await Loader.getResource("images/BGs/frame.png");
+        branches.classes.add("frameLayer");
+
         container.append(branches);
         leaves = await Loader.getResource("images/BGs/frameLeaves.png");
+        leaves.classes.add("frameLayer");
         flowers = await Loader.getResource("images/BGs/frameFlowers.png");
+        flowers.classes.add("frameLayer");
         fruit = await Loader.getResource("images/BGs/frameFruit.png");
+        fruit.classes.add("frameLayer");
         eyes = await Loader.getResource("images/BGs/frameEyes.png");
+        eyes.classes.add("frameLayer");
         buffer = new CanvasElement(width: width, height: height);
     }
 
     Future<Null> render() async {
         if(buffer == null) await initCanvasAndBuffer();
+        buffer.context2D.fillStyle = "#002d4a";
+        buffer.context2D.fillRect(0, 0, buffer.width, buffer.height);
         buffer.context2D.drawImage(bg,0,0);
+        onScreen.context2D.drawImage(buffer, 0,0);
     }
 
 
