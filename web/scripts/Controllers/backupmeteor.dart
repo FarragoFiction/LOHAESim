6528636820e3e5ad3a3aacd1679f2612b30ec1d2
jadedResearcher <gtg287y@gmail.com>
2018-09-04 09:48:30 -0400

    recovery page exists again

--- web/scripts/Controllers/backupmeteor.dart
+++ web/scripts/Controllers/backupmeteor.dart
@@ -10,32 +10,6 @@ Future<Null> main() async{
     await loadNavbar();
     loadBackups();
     saveBackups();
-    changeFPS();
-}
-void changeFPS() {
-    World world = new World();
-    DivElement wrapper = new DivElement()..text = "Change Frames Per Second (at your own peril)";
-    wrapper.style.display = "block";
-    DivElement label = new DivElement()..text = "Lower it from 30 to make older computers run better.";
-    InputElement input = new InputElement();
-    LabelElement label2 = new LabelElement()..text = "${world.musicSave.fps} fps";
-    input.type = "range";
-    input.min = "1";
-    input.max = "60";
-    input.value = "${world.musicSave.fps}";
-
-    wrapper.append(label);
-    wrapper.append(label2);
-    wrapper.append(input);
-    output.append(wrapper);
-
-    input.onChange.listen((Event e) {
-        label2.text = "${input.value} fps";
-        world.musicSave.fps = int.parse(input.value);
-        world.save("changing fps");
-    });
-
-
 }
 
 void loadBackups() {
