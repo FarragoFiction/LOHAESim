ec1e306fc7fa651ac16ded7de2263161b085f982
jadedResearcher <gtg287y@gmail.com>
2019-01-10 20:52:59 -0500

    working on quick body pillow addition

---
+++ web/scripts/Gameplay/Inventoryable/Bodypillow.dart
@@ -0,0 +1,31 @@
+import '../CollectableSecret.dart';
+import '../World.dart';
+import 'Inventoryable.dart';
+import 'dart:async';
+import 'dart:html';
+
+import 'package:RenderingLib/RendereringLib.dart';
+
+//should togggle nidhogg between corurpt and pure states
+class BodyPillow extends CollectableSecret with Inventoryable{
+    @override
+    int sortPriority = 2;
+
+    BodyPillow(World world) : super(world, "You...wonder why anyone would actually sleep with this, and if there is a 'real world' analogue.", "images/BGs/bodypillow.png") {
+        name = "Body Pillow Of Nidhogg";
+        cost = 4037;
+        description = specificPhrase;
+        type = name;
+
+    }
+
+    Future<Null> setCanvasForStore() async{
+        CanvasElement me = new CanvasElement(width:width, height: height);
+        ImageElement myImage = await image;
+        me.context2D.drawImageScaled(myImage, 0, 0, width, height);
+        Renderer.drawToFitCentered(itemCanvas, me);
+        //itemCanvas.context2D.drawImageScaled(me, 0,0,itemCanvas.width, itemCanvas.height);
+    }
+
+
+}
